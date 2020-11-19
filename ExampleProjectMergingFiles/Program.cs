616e6f0c628324734aea3eba47374c7f56fdc671
Jeremy Lindsay <jeremy.lindsay@outlook.com>
2020-11-19 20:06:03 +0000

    added comments to make it a bit easier to understand what i'm doing

--- ExampleProjectMergingFiles/Program.cs
+++ ExampleProjectMergingFiles/Program.cs
@@ -22,39 +22,45 @@ namespace ExampleProjectMergingFiles
                     @".\PDFs\hello-2.pdf"
                 };
 
+                // load the source files into a list of byte arrays
                 var pages = new List<byte[]>();
                 foreach (var filename in fileNames)
                 {
                     pages.Add(File.ReadAllBytes(filename));
                 }
 
+                // merge the list of byte arrays into a single byte array
                 var mergedArray = MergePages(pages);
 
+                // take a note of the size of the merged array the first time we go through the loop
                 if (i == 0)
                 {
                     sizeOfMergedFileFirstTimeRound = mergedArray.Length;
                 }
 
+                // compare the size of the merged file byte array with what it was the first time through the loop
+                // would expect the byte array sizes to always be the same because we're always running the same code.
                 if (mergedArray.Length == sizeOfMergedFileFirstTimeRound)
                 {
                     Console.WriteLine("worked - size = " + mergedArray.Length);
                 }
                 else
                 {
+                    // but 5% of the time, the byte array of the merged files is 2 bytes smaller
                     Console.WriteLine("odd size - size = " + mergedArray.Length + " (i = " + i + ")");
                 }
             }
         }
 
-        public static byte[] MergePages(List<byte[]> pages)
+        public static byte[] MergePages(List<byte[]> pdfByteArrays)
         {
             byte[] parentDocumentContents = Array.Empty<byte>();
 
-            using (var parentStream = new MemoryStream())
+            using (var mergedStream = new MemoryStream())
             {
-                using (var document = new PdfDocument())
+                using (var mergedDocument = new PdfDocument())
                 {
-                    foreach (var pdfByteArray in pages)
+                    foreach (var pdfByteArray in pdfByteArrays)
                     {
                         using (var pdfStream = new MemoryStream())
                         {
@@ -62,13 +68,14 @@ namespace ExampleProjectMergingFiles
 
                             using (var source = PdfDocument.Load(pdfStream))
                             {
-                                document.Pages.AddClone(source.Pages[0]);
+                                // in this sample each file is only one page long, so it's ok to add the first page of the source PDF document
+                                mergedDocument.Pages.AddClone(source.Pages[0]);
                             }
                         }
                     }
 
-                    document.Save(parentStream);
-                    parentDocumentContents = parentStream.ToArray();
+                    mergedDocument.Save(mergedStream);
+                    parentDocumentContents = mergedStream.ToArray();
                 }
             }
 
